6982fb930a0b67135695ded5daa6a3581bafa332
Paul Freck <Paulfreck3@gmail.com>
2023-11-04 00:37:46 -0500

    functional version now with server and user.js

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const fs = require('node:fs');
+const path = require('node:path');
+//These are generally to fine the commands directory and import our commands
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+
+client.commands = new Collection(); //allows the rest of the progress to get commands stored in a collection
+
+const commandsPath = path.join(__dirname, 'commands'); //gets all files in commands
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); //nothing but js gets added
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -13,4 +33,26 @@ client.once(Events.ClientReady, c => {
 
 // Log in to Discord with your client's token
 client.login(token);
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	//beyond this calls the execute() func of the relevant function when a command is created, throwing errors described below
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+}); 
 
