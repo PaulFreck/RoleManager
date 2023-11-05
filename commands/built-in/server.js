dcbdf29d7a736ab0c5d9fa1f28b988f504dd218e
Paul Freck <Paulfreck3@gmail.com>
2023-11-05 01:41:32 -0600

    now adds prints out the person with the role that is provided

--- commands/built-in/server.js
+++ commands/built-in/server.js
@@ -3,20 +3,24 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
-		.setDescription('Provides information about the server.'),
+		.setDescription('Provides information about the server.')
+		.addRoleOption(option =>
+			option.setName('input')
+				.setDescription('default')
+				.setRequired(true)),
 	async execute(interaction) {
-		// interaction.guild is the object representing the Guild in which the command was run
-        //interaction.guild.id
+		const target = interaction.options.getRole('input').name;
 
-		// Get the Guild and store it under the variable "list"
 		const roles = interaction.guild.roles.cache; 
-		//console.log(interaction.guild.roles.cache);
-
 		roles.forEach(role => {
+			console.log(role.name)
+			console.log(target)
+			console.log(target === role.name)
 			//impliment a way to check if this equal to the input, prob if input.name == role.name
 			const members = role.members;
-			//console.log(role.members);
-			members.forEach(member => console.log(member)) //eventually add the members to smth to print out.
+			if (target === role.name){
+				members.forEach(member => console.log(member)) //eventually add the members to smth to print out.
+			}
 		}); 
 
 		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
