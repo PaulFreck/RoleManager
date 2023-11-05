0642600f0bc62ea3c6246da3ba6b7102e05273ec
Paul Freck <Paulfreck3@gmail.com>
2023-11-05 11:48:16 -0600

    working early version, might add @'s soon.

--- commands/built-in/server.js
+++ commands/built-in/server.js
@@ -4,25 +4,55 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
 		.setDescription('Provides information about the server.')
-		.addRoleOption(option =>
-			option.setName('input')
-				.setDescription('default')
-				.setRequired(true)),
+		.addRoleOption(optionone =>
+			optionone.setName('roleone')
+				.setDescription('the first ')
+				.setRequired(true))
+		.addRoleOption(optiontwo =>
+			optiontwo.setName('roletwo')
+				.setDescription('the 2nd role you want to compare to')),
 	async execute(interaction) {
-		const target = interaction.options.getRole('input').name;
+		const target = interaction.options.getRole('roleone').name;
+		console.log(target);
+		const target2 = interaction.options.getRole('roletwo') ?? null;
+		console.log(target2);
+		if (target2 !== null){
+			target2 = target2.name;
+		}
 
+		const targetArray = [];
+		const target2Array = [];
+		const finalArray = [];
 		const roles = interaction.guild.roles.cache; 
 		roles.forEach(role => {
-			console.log(role.name)
-			console.log(target)
 			console.log(target === role.name)
-			//impliment a way to check if this equal to the input, prob if input.name == role.name
 			const members = role.members;
 			if (target === role.name){
-				members.forEach(member => console.log(member)) //eventually add the members to smth to print out.
+				members.forEach(member => {
+					targetArray.push(member.user.globalName);
+					console.log("test!");
+					console.log(target2 === null);
+					if(target2 === null || target2Array.includes(member.user.globalName) ) {
+						finalArray.push(member.user.globalName);
+						console.log(member.user.globalName);
+					}})
+			}
+			if ( target2 !== null && target2 === role.name){
+				members.forEach(member => {
+					targetArray.push(member.user.globalName)
+					if(targetArray.includes(member.user.globalName) ) {
+						finalArray.push(member.user.globalName);
+					}}) 
 			}
 		}); 
-
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		var string = null;
+		console.log(finalArray);
+		if (finalArray !== null){
+			 string = finalArray.join(',');
+		}
+		else{
+			string = "No such user has both roles";
+		}
+		await interaction.reply(string);
 	},
 };
