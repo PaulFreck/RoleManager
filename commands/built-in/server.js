773588289fbb7502f80e239e5a50c713a9d64c84
Paul Freck <Paulfreck3@gmail.com>
2023-11-05 00:06:44 -0500

    exploring ways to get the info we want

--- commands/built-in/server.js
+++ commands/built-in/server.js
@@ -6,7 +6,18 @@ module.exports = {
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
 		// interaction.guild is the object representing the Guild in which the command was run
-        //console.log(keys(interaction.guild))
+        //interaction.guild.id
+
+		// Get the Guild and store it under the variable "list"
+		const roles = interaction.guild.roles.cache; 
+		//console.log(interaction.guild.roles.cache);
+
+		roles.forEach(role => {
+			//impliment a way to check if this is apart of the 
+			const members = role.members.cache
+			members.forEach(member => console.log(member))
+		}); 
+
 		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
 	},
 };
