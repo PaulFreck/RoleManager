6cb8e8c0cb49d044ef81503cdf8626f451855a98
Paul Freck <Paulfreck3@gmail.com>
2023-11-06 00:32:50 -0600

    cleaned up code and added comments

--- commands/built-in/hasRoles.js
+++ commands/built-in/hasRoles.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('hasRoles')
-		.setDescriptiserveron('takes one or two roles and provides all users with those roles.')
+		.setName('hasroles')
+		.setDescription('takes one or two roles and provides all users with all those roles.')
 		.addRoleOption(optionone =>
 			optionone.setName('roleone')
 				.setDescription('the first ')
@@ -12,50 +12,39 @@ module.exports = {
 			optiontwo.setName('roletwo')
 				.setDescription('the 2nd role you want to compare to')),
 	async execute(interaction) {
-		const target = interaction.options.getRole('roleone').name;
-		//console.log(target);
-		var target2 = interaction.options.getRole('roletwo') ?? null;
-		//console.log(target2);
+		const target = interaction.options.getRole('roleone').name; //inputs from above
+		var target2 = interaction.options.getRole('roletwo') ?? null; //default value for target2 is null
 		if (target2 !== null){
-			target2 = target2.name;
+			target2 = target2.name;  //prevents trying to get .name from a null value
 		}
 
-		const targetArray = [];
-		const target2Array = [];
+		const targetArray = []; 
+		const target2Array = []; //these can honestly be swapped for one array and will be if i need to make more than 2 roles to compare for this func
 		const finalArray = [];
 		const roles = interaction.guild.roles.cache; 
-		//roles.then(roleCollection => { //resolve promise getting a collection in role
-			roles.each(role =>{ //with said collection iterate through them
-				//console.log(target === role.name)
-				const members = role.members; //get a cached collection of their members
-				console.log(role.name);
-				if (target === role.name){ 
-					members.each(member => {
-						//console.log(member);
-						targetArray.push(member.user.toString());
-						//console.log("test!");
-						//console.log(target2 === null);
-						if(target2 === null || target2Array.includes(member.user.toString()) ) {
-							finalArray.push(member.user.toString());
-							//console.log(member.user.tag);
-					}})
-				}
-				if ( target2 !== null && target2 === role.name){
-					members.each(member => {
-						targetArray.push(member.user.toString())
-						if(targetArray.includes(member.user.toString()) ) {
-							finalArray.push(member.user.toString());
-					}}) 
+		roles.each(role =>{ //iterate through the cache of roles (there might be a limit to the roles this cache stores which will blow this up, but I can't really test this until later)
+			const members = role.members; //get a collection of members with that role
+			if (target === role.name){ 
+				members.each(member => {
+					targetArray.push(member.user.toString());
+					if(target2 === null || target2Array.includes(member.user.toString()) ) {
+							finalArray.push(member.user.toString()); //iterate through them and if target2Array has them as well, if so, push to finalArray which will eventually be returned
+				}})
 			}
-		}); 
+			if ( target2 !== null && target2 === role.name){
+				members.each(member => {
+					targetArray.push(member.user.toString())
+					if(targetArray.includes(member.user.toString()) ) { //iterate through them and if targetArray has them as well, if so, push to finalArray which will eventually be returned
+						finalArray.push(member.user.toString());
+				}}) 
+		}}); 
 		var string = null;
-		//console.log(finalArray);
-		if (finalArray.length !== 0){
-			 string = finalArray.join(', ');
+		if (finalArray.length !== 0){ 
+			string = finalArray.join(', '); //@them (as that's what's stored in the list) in a pretty way, might get a bit bulky if you include a lot of people here, but not much i can do
 		}
-		else{
+		else{ //if no entries where found return a custom message
 			string = "No such user has these roles";
 		}
 		await interaction.reply(string);
-	},
-};
+		},
+	};
