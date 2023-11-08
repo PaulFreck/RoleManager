61c3198df3818e5c871eb05aeec05842880ac2f9
Paul Freck <Paulfreck3@gmail.com>
2023-11-08 17:57:20 -0600

    non-functional first version of addRoles

--- commands/built-in/addRoles.js
+++ commands/built-in/addRoles.js
@@ -2,20 +2,26 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('hasroles')
+		.setName('addroles')
 		.setDescription('takes one or two roles and provides all users with all those roles.')
 		.addRoleOption(optionone =>
-			optionone.setName('roleToAdd')
+			optionone.setName('roletoadd')
 				.setDescription('The role that you want to add to the users')
 				.setRequired(true))
 		.addUserOption(optiontwo =>
-			optiontwo.setName('userOne')
+			optiontwo.setName('userone')
 				.setDescription('the first user to add the role to')
                 .setRequired(true)),
 	async execute(interaction) {
-		const role = interaction.options.getRole('roleToAdd'); //inputs from role above
-		const target = interaction.options.getUser('userOne'); //inputs from user above
-        target.roles.add(role);
+		const role = interaction.options.getRole('roletoadd'); //inputs from role above
+		const target = interaction.options.getUser('userone'); //inputs from user above
+        const userPromise = interaction.guild.members.fetch(target.id);
+            userPromise.then(user => {
+            user.roles.addRole(role.id)
+        });
+        console.log(target.constructor.name);
+        console.log(role.constructor.name);
+        //target.roles.add(role);
 		await interaction.reply("It has been done.");
 		},
 	};
